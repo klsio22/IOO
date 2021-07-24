a66aa3b2aef8309b952c412146f410508be3aa99
klesio25 <klesio.k9@gmail.com>
2021-07-24 01:56:31 -0300

    finished EX-Bilhetadora L3 IOO

--- src/Exlista_IOO_03/Bilhetadora/Bilhetadora.java
+++ src/Exlista_IOO_03/Bilhetadora/Bilhetadora.java
@@ -2,36 +2,56 @@ package Exlista_IOO_03.Bilhetadora;
 
 public class Bilhetadora {
     private double preco;
+    private double valor;
+    private double totalArrecadado;
 
-    public Bilhetadora(double preco){
+    public Bilhetadora(double preco) {
         setPreco(preco);
     }
 
     public void setPreco(double preco) {
-        this.preco = preco;
+        if (preco > 0)
+            this.preco = preco;
     }
 
     public double getPreco() {
         return preco;
     }
 
-    public void restituirSaldo(){
-
+    public double restituirSaldo() {
+        double devolucao = this.valor;
+        this.valor -= this.valor;
+        return devolucao;
     }
 
 
-    public void inserir(double preco){
+    public void inserir(double valor) {
+        if (valor > 0)
+            this.valor += valor;
     }
 
-    public String getBilhete(){
+    public String getBilhete() {
+        String bilhete;
+
+        if (getSaldo() != 0) {
+            this.valor -= preco;
+            totalArrecadado += preco;
+            bilhete = "##################";
+            bilhete += "# The BlueJ Line";
+            bilhete += "# Ticket";
+            bilhete += "# " + getPreco() + " cents.";
+            bilhete += "##################";
+            return bilhete;
+        }
+
         return "";
     }
 
-    public double getSaldo(){
-        return 0.0;
-    }
-    public double getTotalArrecadado(){
-        return 0.0;
+    public double getSaldo() {
+        return valor;
     }
 
+    public double getTotalArrecadado() {
+        return totalArrecadado;
+    }
 }
--- src/Exlista_IOO_03/Bilhetadora/TesteBilhetadora.java
+++ src/Exlista_IOO_03/Bilhetadora/TesteBilhetadora.java
@@ -6,104 +6,103 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TesteBilhetadora {
-	
-	private static final double DELTA = 0.001;
-	private Bilhetadora bilhetadora;
 
-	@BeforeEach
-	public void antesDeCadaTeste() {
-		bilhetadora = new Bilhetadora(50);
-	}
+    private static final double DELTA = 0.001;
+    private Bilhetadora bilhetadora;
 
-	@Test
-	public void deveSerPossivelDefinirOPrecoDoBilheteNaInstanciacao() {
-		assertEquals(50, bilhetadora.getPreco(), DELTA);
-	}
-	
-	@Test
-	public void deveSerPossivelAlterarOPrecoDoBilhete() {
-		bilhetadora.setPreco(10);
-		assertEquals(10, bilhetadora.getPreco(), DELTA);
-	}
+    @BeforeEach
+    public void antesDeCadaTeste() {
+        bilhetadora = new Bilhetadora(50);
+    }
 
-	@Test
-	public void naoDeveAceitarValoresNegativosParaOPreco() {
-		bilhetadora.setPreco(-10);
-		assertEquals(50, bilhetadora.getPreco(), DELTA);
-		
-		bilhetadora = new Bilhetadora(-50);
-		assertEquals(0, bilhetadora.getPreco(), DELTA);
-	}
-	
-	@Test
-	public void deveTerSaldoETotalArrecadoIgualaZero() {
-		assertEquals(0, bilhetadora.getSaldo(), DELTA);
-		assertEquals(0, bilhetadora.getTotalArrecadado(), DELTA);
-	}
-	
-	@Test
-	public void deveInserirDinheiro() {
-		bilhetadora.inserir(10);
-		bilhetadora.inserir(15);
-		
-		assertEquals(25, bilhetadora.getSaldo(), DELTA);
-	}
+    @Test
+    public void deveSerPossivelDefinirOPrecoDoBilheteNaInstanciacao() {
+        assertEquals(50, bilhetadora.getPreco(), DELTA);
+    }
 
-	@Test
-	public void naoDeveAceitarValoresNegativos() {
-		bilhetadora.inserir(10);
-		bilhetadora.inserir(-15);
-		
-		assertEquals(10, bilhetadora.getSaldo(), DELTA);
-	}
-	
-	@Test
-	public void deveSerPossivelRestituirOValorInserido() {
-		bilhetadora.inserir(20);
-		
-	//	assertEquals(20, bilhetadora.restituirSaldo(), DELTA);
-		assertEquals(0, bilhetadora.getSaldo(), DELTA);
-	}
-	
-	@Test
-	public void deveRetornarOBilheteQuandoTiverSaldo() {
-		bilhetadora.inserir(50);
-		String bilhete =  "##################";
-		       bilhete += "# The BlueJ Line";
-		       bilhete += "# Ticket";
-		       bilhete += "# " + bilhetadora.getPreco() + " cents.";
-		       bilhete += "##################";
-        
-		assertEquals(bilhete, bilhetadora.getBilhete());       
-	}
-	
-	@Test
-	public void naoDeveRetornarOBilheteSeNaoTiverSaldo() {
-		assertEquals("", bilhetadora.getBilhete()); 
-	}
-	
-	@Test
-	public void deveDiminuirOSaldoAoRetornarOBilhete() {
-		bilhetadora.inserir(120);
-		
-		assertEquals(120, bilhetadora.getSaldo(), DELTA);
-		bilhetadora.getBilhete();
-		
-//		assertEquals(70, bilhetadora.restituirSaldo(), DELTA);
-		assertEquals(0, bilhetadora.getSaldo(), DELTA);
-	}
-	
-	@Test
-	public void deveIncrementarOValorArrecadoACadaBilheteVendido() {
-		bilhetadora.inserir(120);
-		bilhetadora.getBilhete();
-		assertEquals(50, bilhetadora.getTotalArrecadado(), DELTA);
-		bilhetadora.getBilhete();
-		assertEquals(100, bilhetadora.getTotalArrecadado(), DELTA);
-		
-		assertEquals(20, bilhetadora.getSaldo(), DELTA);
-		//assertEquals(20, bilhetadora.restituirSaldo(), DELTA);
-		assertEquals(0, bilhetadora.getSaldo(), DELTA);
-	}
-	
+    @Test
+    public void deveSerPossivelAlterarOPrecoDoBilhete() {
+        bilhetadora.setPreco(10);
+        assertEquals(10, bilhetadora.getPreco(), DELTA);
+    }
+
+    @Test
+    public void naoDeveAceitarValoresNegativosParaOPreco() {
+        bilhetadora.setPreco(-10);
+        assertEquals(50, bilhetadora.getPreco(), DELTA);
+
+        bilhetadora = new Bilhetadora(-50);
+        assertEquals(0, bilhetadora.getPreco(), DELTA);
+    }
+
+    @Test
+    public void deveTerSaldoETotalArrecadoIgualaZero() {
+        assertEquals(0, bilhetadora.getSaldo(), DELTA);
+        assertEquals(0, bilhetadora.getTotalArrecadado(), DELTA);
+    }
+
+    @Test
+    public void deveInserirDinheiro() {
+        bilhetadora.inserir(10);
+        bilhetadora.inserir(15);
+
+        assertEquals(25, bilhetadora.getSaldo(), DELTA);
+    }
+
+    @Test
+    public void naoDeveAceitarValoresNegativos() {
+        bilhetadora.inserir(10);
+        bilhetadora.inserir(-15);
+
+        assertEquals(10, bilhetadora.getSaldo(), DELTA);
+    }
+
+    @Test
+    public void deveSerPossivelRestituirOValorInserido() {
+        bilhetadora.inserir(20);
+        assertEquals(20, bilhetadora.restituirSaldo(), DELTA);
+        assertEquals(0, bilhetadora.getSaldo(), DELTA);
+    }
+
+    @Test
+    public void deveRetornarOBilheteQuandoTiverSaldo() {
+        bilhetadora.inserir(50);
+        String bilhete = "##################";
+        bilhete += "# The BlueJ Line";
+        bilhete += "# Ticket";
+        bilhete += "# " + bilhetadora.getPreco() + " cents.";
+        bilhete += "##################";
+
+        assertEquals(bilhete, bilhetadora.getBilhete());
+    }
+
+    @Test
+    public void naoDeveRetornarOBilheteSeNaoTiverSaldo() {
+        assertEquals("", bilhetadora.getBilhete());
+    }
+
+    @Test
+    public void deveDiminuirOSaldoAoRetornarOBilhete() {
+        bilhetadora.inserir(120);
+
+        assertEquals(120, bilhetadora.getSaldo(), DELTA);
+        bilhetadora.getBilhete();
+
+        assertEquals(70, bilhetadora.restituirSaldo(), DELTA);
+        assertEquals(0, bilhetadora.getSaldo(), DELTA);
+    }
+
+    @Test
+    public void deveIncrementarOValorArrecadoACadaBilheteVendido() {
+        bilhetadora.inserir(120);
+        bilhetadora.getBilhete();
+        assertEquals(50, bilhetadora.getTotalArrecadado(), DELTA);
+        bilhetadora.getBilhete();
+        assertEquals(100, bilhetadora.getTotalArrecadado(), DELTA);
+
+        assertEquals(20, bilhetadora.getSaldo(), DELTA);
+        assertEquals(20, bilhetadora.restituirSaldo(), DELTA);
+        assertEquals(0, bilhetadora.getSaldo(), DELTA);
+    }
+
 }
